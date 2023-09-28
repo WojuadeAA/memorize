3becb1207403bd89ef693100800bd8f774415b8a
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2023-09-28 01:04:17 +0100

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Wojuade Abdul Afeez on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
